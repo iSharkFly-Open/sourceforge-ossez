5ed0622ccfa19408f3ba5a54c0a2c0291ea6bcc5
YuCheng Hu <yhu@isharkfly.com>
2010-01-24 01:26:04 +0000

    +YUCHENG HU+
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@2 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ mainweb/trunk/index.php
@@ -0,0 +1,14 @@
+<html>
+<title>HA WEBSYSTEMS</title>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="expires" content="Tue, 03 Jan 1980 1:00:00 GMT">
+<meta http-equiv="refresh" content="0;url=http://www.tcivis.com/cuist/default.php">
+
+<link href="css/public.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+Loading...
+</body>
+</html>
