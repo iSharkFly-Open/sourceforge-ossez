bbee4c172c316a439f8a4def03fbb7b1eb7c21f8
YuCheng Hu <yhu@isharkfly.com>
2010-01-31 03:59:55 +0000

    第一次添加源代码到代码库。 +YUCHENG HU+
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@12 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ mainweb/trunk/repository/svn/svn_nav.php
@@ -0,0 +1,26 @@
+<div style="background:url(/assets/img/interface/new_undershadow.gif) left top repeat-x #e4e0dd;"> <br class="clearboth" />
+  <div id="nav-more" class="clearfix">
+    <div class="t">
+      <div class="b">
+        <div class="l">
+          <div class="r">
+            <div class="bl">
+              <div class="br" style="margin:0">
+                <div class="tl">
+                  <div class="tr" style="height:15px">
+                    <h2 style="text-transform:none;font-size:1em;float:left;padding:0;width:100%;" class="fix_floats">
+                      <div class="clearboth float_right"> <img src="<?php echo WEB_PATH; ?>/assets/img/global_elements/latestnews_rss.jpg" alt="Rss" width="12" height="12" style="margin-bottom:-2px" /><a href="/page.cfm?section=rss" title="RSS" style="color:#6c655f" rel="nofollow" onClick="s.linkTrackVars='prop6'; s.prop6='Header'; s.tl(true,'o','Header');">RSS新闻</a> &nbsp; <img src="<?php echo WEB_PATH; ?>/assets/img/interface/articleEnvelope.gif" alt="Newsletter Signup" width="13" height="12" style="margin-bottom:-2px" /> <a href="/page.cfm?section=newslettersignup&amp;OriginCode=20080430nav" rel="nofollow" title="Newsletter Signup" style="color:#6c655f" onClick="s.linkTrackVars='prop6'; s.prop6='Header'; s.tl(true,'o','Header');">新闻订阅</a> &nbsp; <img src="<?php echo WEB_PATH; ?>/assets/img/interface/bookmark.gif" alt="Bookmark" width="13" height="12" style="margin-bottom:-2px" /> <a href="javascript:bookmarkpage();" rel="nofollow" title="Bookmark" style="color:#6c655f" onClick="s.linkTrackVars='prop6'; s.prop6='Header'; s.tl(true,'o','Header');">存为书签</a> </div>
+                      <div class="float_left" style="width:625px"><a href="http://www.hawebs.net">Hawebs.net</a> &nbsp; &gt; &nbsp; SVN库</div>
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- // rounded corners - breadcrumbs -->
+  </div>
+  <!--/end nav more options-->
+</div>
