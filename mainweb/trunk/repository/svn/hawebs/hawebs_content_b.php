196599e973244be0542be41a41dbb0b5137028fd
YuCheng Hu <yhu@isharkfly.com>
2010-01-31 04:00:30 +0000

    第一次添加源代码到代码库。 +YUCHENG HU+
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@13 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ mainweb/trunk/repository/svn/hawebs/hawebs_content_b.php
@@ -0,0 +1,52 @@
+<div id="content_b">
+  <div class="rightNav section_b">
+    <div>
+      <h2>Hawebs.net&nbsp;&nbsp;维护的项目</h2>
+    </div>
+    <p class="rightNavStyle2"><span>HAWEBS网站主程序更新存档。</span><br />
+      <a href="http://sourceforge.net/projects/hawebs/">Hawebs主程序</a></p>
+    <p class="rightNavStyle2 even"><span>Haebl项目，支持图书数据的信息化和共享。</span><br />
+      <a href="http://sourceforge.net/projects/haebl/">Haebl图书数据处理平台</a></p>
+    <p class="rightNavStyle2"><span>代码正在提交</span><br />
+      <a href="#">代码正在提交</a></p>
+    <p class="rightNavStyle2 even"><span>代码正在提交</span><br />
+      <a href="#">代码正在提交</a></p>
+    <p class="rightNavStyle2" style="border-bottom: none !important"><span>代码正在提交</span><br />
+      <a href="#">代码正在提交</a></p>
+  </div>
+  <script type="text/javascript">
+		  // var SCALinks=new Array();
+		  // SCALinks[0]="https://w1.buysub.com/loc/SCA/IASBSR7";
+		  // SCALinks[1]="https://w1.buysub.com/loc/SCA/IASBSA7";
+		  // SCALink = SCALinks[Math.floor(Math.random()*(SCALinks.length))];
+		  SCALink ="https://w1.buysub.com/loc/SCA/IASBSA7";
+
+		  var SCAGift ="https://w1.buysub.com/loc/SCA/IASBSG1";
+
+		  var MNDLinks=new Array();
+		  MNDLinks[0]="https://w1.buysub.com/loc/MND/IAMBSR8";
+		  //MNDLinks[1]="https://w1.buysub.com/loc/MND/IAMBSR3";
+		  MNDLink = MNDLinks[Math.floor(Math.random()*(MNDLinks.length))];
+
+		  var MNDGift ="https://w1.buysub.com/loc/MND/IAMBSG1";
+	</script>
+  <br class="clearboth"/>
+  <div class="advertise" align="center"> Advertisement
+    <script type="text/javascript">OAS_AD("Right1");</script>
+  </div>
+  <br class="clearboth" />
+  <!--/end advertise-->
+  <div class="rightNav section_b">
+    <div>
+      <h2>赞助商</h2>
+    </div>
+  </div>
+  <div id="pop_news">
+    <p class=""><a href="http://www.apache.org/" target="_blank" title="Apache软件基金会" rel="nofollow">Apache软件基金会</a></p>
+    <p class="even"><a href="http://www.logoworldwide.com/" target="_blank" title="logo Worldwide - 进出口贸易供应商" rel="nofollow">logo Worldwide - 进出口贸易供应商</a></p>
+    <p class=""><a href="http://www.mysonicfire.com/" target="_blank" title="SonicFire Crop." rel="nofollow">SonicFire Crop.</a></p>
+    <p class="even"><a href="http://www.bookyard.com.cn/" target="_blank" title="书源 - 海天书目信息服务" rel="nofollow">书源 - 海天书目信息服务</a></p>
+  </div>
+  <br class="clearboth" />
+  <!--/end Sponsors-->
+</div>
