ee7cd560e5f30e9eb2de41bf7178a7a0c6bc10df
YuCheng Hu <yhu@isharkfly.com>
2010-06-07 20:05:44 +0000

    中文语言包
    +YUCHENG HU+
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@101 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/joomla/trunk/language/zh-CN/zh-CN.ignore.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright (C) 2006-2009 YUCHENG HU
+ *
+ * ----------------------------------------------------------------------------------
+ * HA SYSTEMS
+ * http://www.tcivis.com
+ * http://www.hawebs.org
+ * http://www.hawebs.net
+ *
+ * Contact
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ----------------------------------------------------------------------------------
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * GNU GENERAL PUBLIC LICENSE
+ *
+ * ----------------------------------------------------------------------------------
+ */
+
+$search_ignore[] = "和";
+$search_ignore[] = "内";
+$search_ignore[] = "上";
+?>
