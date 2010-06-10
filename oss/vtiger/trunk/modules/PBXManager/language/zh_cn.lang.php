99825fef8a38d46e3c2155b184b9257b091feea2
YuCheng Hu <yhu@isharkfly.com>
2010-06-10 18:48:29 +0000

    添加到 trunk
    +YUCHENG HU+
    
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@109 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/PBXManager/language/zh_cn.lang.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+
+$mod_strings = Array(
+	'Asterisk' => 'Asterisk',
+	'LBL_ASTERISK_INFORMATION' => 'ASTERISK 信息',
+);
+
+?>
