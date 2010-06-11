33a53567f533498603048108c332f9f6a839fd03
YuCheng Hu <yhu@isharkfly.com>
2010-06-11 02:35:18 +0000

    添加到 trunk
    +YUCHENG HU+
    
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@124 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/user_privileges/user_privileges_6.php
@@ -0,0 +1,9 @@
+<?php
+
+
+//This is the access privilege file
+$is_admin=true;
+
+$user_info=array('user_name'=>'huyucheng','is_admin'=>'on','user_password'=>'$1$hu$paNaux7B01hcVkxBxTiSU1','confirm_password'=>'$1$hu$paNaux7B01hcVkxBxTiSU1','first_name'=>'','last_name'=>'HU','roleid'=>'H2','email1'=>'yuchenghu@hawebs.net','status'=>'Active','activity_view'=>'Today','lead_view'=>'Today','currency_id'=>'1','currency_name'=>'USA, Dollars','currency_code'=>'USD','currency_symbol'=>'&#36;','conv_rate'=>'1.000','hour_format'=>'','end_hour'=>'','start_hour'=>'','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','yahoo_id'=>'','phone_home'=>'','imagename'=>'','date_format'=>'dd-mm-yyyy','signature'=>'','description'=>'','reminder_interval'=>'None','internal_mailer'=>'1','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','asterisk_extension'=>'','use_asterisk'=>'1','accesskey'=>'YGXuaw8TWk0ROTL','record_id'=>'','record_module'=>'','id'=>'6');
+
+?>
