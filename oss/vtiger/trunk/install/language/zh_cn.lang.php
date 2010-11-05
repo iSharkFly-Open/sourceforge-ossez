921bb8da21c743ae6a00ac134cc88956f1fea3fb
YuCheng Hu <yhu@isharkfly.com>
2010-11-05 06:39:52 +0000

    更新到 5.2 后第一次 SVN 提交。
    新增安装语言文件。
    
    yuchenghu@hawebs.net
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@564 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/install/language/zh_cn.lang.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$optionalModuleStrings = array(
+		'CustomerPortal_description'=>'Management interface to control the behavior of Customer Portal Plugin',
+		'FieldFormulas_description'=>'Setup rules for custom fields to update value on record save',
+		'RecycleBin_description'=>'Module to manage deleted records, provides ability to restore or remove it completely',
+		'Tooltip_description'=>'Configure tooltip to be shown for a field, which can be combination of other fields',
+		'Webforms_description'=>'Server side support to allow building client webforms to capture the information easily',
+	);
+?>
