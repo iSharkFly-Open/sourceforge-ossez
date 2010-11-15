d109e7cc168d5f97ba4f1dc3c50ea5a5960b0b78
YuCheng Hu <yhu@isharkfly.com>
2010-11-15 09:01:11 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@580 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

--- oss/vtiger/trunk/modules/Assets/language/zh_cn.lang.php
+++ oss/vtiger/trunk/modules/Assets/language/zh_cn.lang.php
@@ -1,42 +1,54 @@
 <?php
-/*+**********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- ************************************************************************************/
-
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * http://forums.hawebs.net/computer/index.php
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
 $mod_strings = Array(
 /*some general information*/
-'LBL_MODULE_NAME'=>'Assets',
-'SINGLE_Assets'=>'Asset',
+'LBL_MODULE_NAME'=>'资产',
+'SINGLE_Assets'=>'资产',
 
 /*blocks for the module*/
-'LBL_ASSET_INFORMATION'=>'Asset Information',
-'LBL_CUSTOM_INFORMATION'=>'Custom Information',
-'LBL_DESCRIPTION_INFORMATION'=>'Notes',
+'LBL_ASSET_INFORMATION'=>'资产信息',
+'LBL_CUSTOM_INFORMATION'=>'用户信息',
+'LBL_DESCRIPTION_INFORMATION'=>'备注',
 
 /*fields for the module*/
-'Assets'=>'Assets',
-'Asset Name' => 'Asset Name',
-'Customer Name'=>'Customer Name',
-'Product Name'=>'Product Name',
-'Serial Number'=>'Serial Number',
-'Asset No'=>'Asset No',
-'Date Sold'=>'Date Sold',
-'Date in Service'=>'Date in Service',
-'Status'=>'Status',
-'Shipping Method'=>'Shipping Method',
-'Shipping Tracking Number'=>'Shipping Tracking Number',
-'Tag Number'=>'Tag Number',
-'Notes'=>'Notes',
-'Invoice Name'=>'Invoice Name',
+'Assets'=>'资产',
+'Asset Name' => '资产名称',
+'Customer Name'=>'客户名称',
+'Product Name'=>'产品名称',
+'Serial Number'=>'序列号',
+'Asset No'=>'资产编号',
+'Date Sold'=>'销售日期',
+'Date in Service'=>'服务日期',
+'Status'=>'状态',
+'Shipping Method'=>'投递方法',
+'Shipping Tracking Number'=>'投递跟踪单号',
+'Tag Number'=>'标签号',
+'Notes'=>'备注',
+'Invoice Name'=>'发表抬头',
 
 /*picklist values*/
-'In Service'=>'In Service',
-'Out-of-service'=>'Out-of-service',
+'In Service'=>'正在服务',
+'Out-of-service'=>'服务过期',
 
 
 );
