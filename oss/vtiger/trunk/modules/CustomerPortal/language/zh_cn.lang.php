101bc4f32de55bc1f88e8fb3c4cac61da71221e4
YuCheng Hu <yhu@isharkfly.com>
2010-11-16 05:45:14 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@589 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/CustomerPortal/language/zh_cn.lang.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$mod_strings = Array (
+'CustomerPortal' => '客户门户 ',
+'LBL_BASIC_SETTINGS'=>'基本设置',
+'LBL_ADVANCED_SETTINGS'=>'高级设置',
+'LBL_MODULE'=>'模块',
+'LBL_VIEW_ALL_RECORD'=>'查看所有相关记录 ?',
+'YES'=>'是',
+'NO'=>'否',
+'LBL_USER_DESCRIPTION'=>'您可以通过设置来决定上述选定的用户的相关权限，比如字段是否可见，模块是否可用等.',
+'SELECT_USERS'=>'选择用户',				
+'LBL_DISABLE'=>'禁用',
+'LBL_ENABLE' =>'启用',
+'Module' => '模块',
+'Sequence' =>'次序',
+'Visible'=>'可见'
+
+);
+
+?>
