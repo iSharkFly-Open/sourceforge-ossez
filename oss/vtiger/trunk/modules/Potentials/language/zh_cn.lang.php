4d16fc224be5049669f2e9e48f8acfd85eb4a18a
YuCheng Hu <yhu@isharkfly.com>
2010-11-16 15:40:44 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@630 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/Potentials/language/zh_cn.lang.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$mod_strings = Array(
+'LBL_MODULE_NAME' => '业务',
+'LBL_MODULE_TITLE' => '业务： 首页',
+'LBL_SEARCH_FORM_TITLE' => '搜寻业务',
+'LBL_LIST_FORM_TITLE' => '业务列表',
+'LBL_OPPORTUNITY_NAME' => '机会名称：',
+'LBL_OPPORTUNITY' => '机会：',
+'LBL_NAME' => '业务名称',
+'LBL_INVITEE' => '联系人',
+'LBL_LIST_OPPORTUNITY_NAME' => '可能性',
+'LBL_LIST_ACCOUNT_NAME' => '客户名称',
+'LBL_PRODUCT_NAME' => '业务类型',
+'LBL_LIST_AMOUNT' => '合计',
+'LBL_LIST_DATE_CLOSED' => '预期结束',
+'LBL_LIST_SALES_STAGE' => '销售策略',
+
+'LBL_OPPORTUNITY_NAME'=>'潜在客户名称:',
+'LBL_ACCOUNT_NAME' => '客户名称：',
+'LBL_AMOUNT' => '合计：',
+'LBL_DATE_CLOSED' => '预期结束日期：',
+'LBL_TYPE' => '类型：',
+'LBL_NEXT_STEP' => '下一步：',
+'LBL_LEAD_SOURCE' => '接触来源：',
+'LBL_SALES_STAGE' => '客服场景：',
+'LBL_PROBABILITY' => '可能性 (%)：',
+'LBL_DESCRIPTION' => '描述：',
+'LBL_OPPORTUNITY_INFORMATION' => '业务信息：',
+'LBL_DESCRIPTION_INFORMATION' => '描述信息：',
+'LBL_CUSTOM_INFORMATION' => '自订信息',
+
+'LBL_NEW_FORM_TITLE' => '新增业务',
+
+'ERR_DELETE_RECORD' => '删除业务必须指定记录编号.',
+'LBL_TOP_OPPORTUNITIES' => '最佳业务',
+
+'NTC_REMOVE_OPP_CONFIRMATION' => '您确定要从业务中删除这位联系人？',
+
+'NTC_NONE_SCHEDULED' => '未安排行程',
+
+'LBL_SELECT_OPPOPTUNITY' => '选择业务',
+'LBL_GENERAL_INFORMATION' => '一般信息',
+
+
+
+//for v4 release added
+'LBL_NEW_POTENTIAL' => '新增业务',
+'LBL_POTENTIAL_TITLE' => '业务',
+
+'LBL_NEW_TASK' => '新增任务',
+'LBL_TASK_TITLE' => '任务',
+'LBL_NEW_CALL' => '新增电话记录',
+'LBL_CALL_TITLE' => '电话记录',
+'LBL_NEW_MEETING' => '新增会议',
+'LBL_MEETING_TITLE' => '会议',
+'LBL_NEW_EMAIL' => '新增电子邮件',
+'LBL_EMAIL_TITLE' => '电子邮件',
+'LBL_NEW_NOTE' => '新增备忘录',
+'LBL_NOTE_TITLE' => '备忘录',
+
+'LBL_NEW_ATTACHMENT' => '新增附件',
+'LBL_ATTACHMENT_TITLE' => '附件',
+
+'LBL_NEW_CONTACT' => '新增联系人',
+'LBL_CONTACT_TITLE' => '联系人',
+
+//Added vtiger_fields after RC1 - Release
+'LBL_ALL' => '全部',
+'LBL_WON' => '获得',
+'LBL_LOST' => '失去',
+'LBL_VALUE_PROPOSITION' => '价值建议',
+'LBL_PROSPECTING' => '预期值',
+
+// Added for 4GA
+'LBL_TOOL_FORM_TITLE' => '业务编辑器',
+//Added for 4GA
+'Potential Name' => '业务名称',
+'Amount' => '合计',
+'Account Name' => '客户名称',
+'Currency' => '货币',
+'Expected Close Date' => '预定结束日期',
+'Opportunity Type' => '机会类型',
+'Type' => '类型',
+'Next Step' => '下一步',
+'Lead Source' => '接触来源',
+'Sales Stage' => '销售策略',
+'Assigned To' => '负责人',
+'Probability' => '可能性 (%)',
+'Campaign Source' => '活动来源',
+'Description' => '描述',
+'Created Time' => '建立时间',
+'Modified Time' => '修改时间',
+//Added for 4.2 Release -- CustomView
+'Potential' => '业务',
+'Expected Close' => '预期结案日期',
+
+//Added for Existing Picklist Strings
+
+'Prospecting' => '潜在客户',
+'Qualification' => '具有资格',
+'Needs Analysis' => '需要分析',
+'Value Proposition' => '价值主张',
+'Id. Decision Makers' => '决策者',
+'Perception Analysis' => '感觉分析',
+'Proposal/Price Quote' => '建议书/报价单',
+'Negotiation/Review' => '交涉/审阅',
+'Closed Won' => '成功结案',
+'Closed Lost' => '失败结案',
+
+'Cold Call' => '陌生拜访',
+'Existing Customer' => '既有客户',
+'Self Generated' => '自动产生',
+'Employee' => '员工',
+'Partner' => '伙伴',
+'Public Relations' => '公众关系',
+'Direct Mail' => '邮件营销',
+'Conference' => '研讨会',
+'Trade Show' => '贸易展览',
+'Web Site' => '网站',
+'Word of mouth' => '朋友介绍',
+'Other' => '其它',
+
+'--None--' => '-- 无 --',
+'Existing Business' => '已有商务往来',
+'New Business' => '新客户',
+
+// Added after 5.0.4 GA
+
+// Module Sequence Numbering
+'Potential No' => '潜在机会序列号',
+// END
+
+);
+
+?>
