1004bba11a527d6f7341f4dfb0a410253f33ad4a
YuCheng Hu <yhu@isharkfly.com>
2010-11-16 15:14:02 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@628 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/PickList/language/zh_cn.lang.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$mod_strings = array(
+	'LBL_ASSIGN_BUTTON'=>'指定下拉选择',
+	'ADD_PICKLIST_VALUES'=>'添加下拉选择列表变量',
+	'LBL_EXISTING_PICKLIST_VALUES'=>'存在的下拉选择变量',
+	'LBL_PICKLIST_ADDINFO'=>'在这里添加新实体',
+	'LBL_SELECT_ROLES'=>'选择角色',
+	'LBL_NON_EDITABLE_PICKLIST_ENTRIES'=>'不可编辑的变量',
+	'EDIT_PICKLIST_VALUE'=>'编辑下拉选择变量',
+	'LBL_EDIT_HERE'=>'用下面的下拉选择变量替换：',
+	'LBL_SELECT_TO_EDIT'=>'选择一个需要编辑的下拉选择变量：',
+	'DELETE_PICKLIST_VALUES'=>'删除下拉选择列表变量',
+	'LBL_REPLACE_WITH'=>'用下面的下拉选择变量替换：',
+	'ASSIGN_PICKLIST_VALUES'=>'指定下拉选择变量',
+	'LBL_PICKLIST_VALUES'=>'可用下拉选择变量',
+	'LBL_PICKLIST_VALUES_ASSIGNED_TO'=>'指定下拉选择变量为用户角色： ',
+	'LBL_ADD_TO_OTHER_ROLES'=>'为其他角色指定下拉选择变量',
+	'LBL_OK_BUTTON_LABEL'=>'确定',
+	'LBL_SELECT_ROLES'=>'选择角色',
+	'LBL_DISPLAYED_VALUES'=>'下面显示了当前用户角色所有可以被指定的选择变量',
+);
+?>
