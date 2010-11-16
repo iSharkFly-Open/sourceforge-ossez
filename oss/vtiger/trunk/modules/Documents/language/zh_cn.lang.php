50ba8e9ac33d03428306785b57db830a6f3e0bb9
YuCheng Hu <yhu@isharkfly.com>
2010-11-16 06:45:38 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@598 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/Documents/language/zh_cn.lang.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$mod_strings = Array(
+'LBL_MODULE_NAME'=>'文档',
+'LBL_MODULE_TITLE'=>'文档: 首页',
+'LBL_SEARCH_FORM_TITLE'=>'文档查找',
+'LBL_LIST_FORM_TITLE'=>'文档列表',
+'LBL_NEW_FORM_TITLE'=>'新建文档',
+
+'LBL_LIST_SUBJECT'=>'主题',
+'LBL_LIST_CONTACT_NAME'=>'联系人',
+'LBL_LIST_RELATED_TO'=>'指定给',
+'LBL_LIST_DATE_MODIFIED'=>'最后修改日期',
+
+'LBL_NOTE'=>'文档:',
+'LBL_NOTE_SUBJECT'=>'文档主题:',
+'LBL_CONTACT_NAME'=>'联系人:',
+'LBL_PHONE'=>'电话:',
+'LBL_SUBJECT'=>'主题:',
+'LBL_CLOSE'=>'关闭:',
+'LBL_RELATED_TO'=>'指定给:',
+'LBL_EMAIL_ADDRESS'=>'电子邮件地址:',
+'LBL_COLON'=>':',
+
+'ERR_DELETE_RECORD'=>"A record number must be specified to delete the vtiger_account.",
+'LBL_LIST_FILENAME'=>"文件名",
+
+// Added for Note(3.2-patch 3) 
+
+'LBL_FILENAME'=>'附件:',
+'LBL_NOTE_INFORMATION'=>'基本信息',
+'File Information'=>'文件信息',
+
+// Added for 4GA
+'LBL_TOOL_FORM_TITLE'=>'文档工具',
+// Added for 4GA
+'Contact Name'=>'联系人',
+'Related To'=>'指定给',
+'Subject'=>'主题',
+'Created Time'=>'创建日期',
+'Modified Time'=>'修改时间',
+'Document'=>'文档',
+'Note'=>'Notes',
+//Added for 4.2 Release -- CustomView
+'Related to'=>'指定给',
+'Last Modified'=>'最后修改日期',
+'File'=>'文件',
+'LBL_ALL'=>'所有',
+'Title'=>'标题',
+
+//Added after 504 for Downloads Module
+'LBL_FILE_INFORMATION'=>'文件信息',
+'LBL_FILE_NAME'=>'文件名',
+'LBL_FILE_TYPE'=>'文件类型',
+'LBL_FILE_SIZE'=>'大小',
+'filelocationtype'=>'下载类型',
+'fileversion'=>'版本',
+'filestatus'=>'归档',
+'filedownloadcount'=>'下载量',
+'os'=>'适用平台',
+'LBL_FOLDER_NAME'=>'文件夹名',
+'LBL_DOWNLOAD_TYPE'=>'下载类型',
+'LBL_INTERNAL'=>'内部链接',
+'LBL_EXTERNAL'=>'外部链接',
+'LBL_EXTERNAL_FILE_NAME'=>'文件名',
+'LBL_FILE_LOCATION'=>'文件位置',
+'LBL_PD'=>'平台相关',
+'LBL_PIND'=>'平台无关',
+'ChoosePlatform'=>'选择平台',
+'LBL_ACTIVE'=>'活动',
+'LBL_INACTIVE'=>'非活动',
+'LBL_UPLOAD_BUTTON'=>'上传',
+'architecture'=>'归档',
+'LBL_DOWNLOAD_FILE'=>'下载文件',
+'LBL_CHECK_INTEGRITY'=>'检查文件完整性',
+'LBL_EMAIL_FILE'=>'作为电子邮件附件发送这个文件',
+'LBL_CLICK_HERE'=>'单击这里',
+'LBL_TO_ADD_FOLDER'=>'新建一个目录.',
+'LBL_DELETE_FOLDER'=>'删除目录',
+'LBL_FOLDER_DESC'=>'描述',
+'LBL_ADD_NEW_FOLDER'=>'添加目录',
+'LBL_DELETE_DOCUMENTS'=>'删除文档',
+'LBL_MOVE_TO'=>'移动到',
+'File Name'=>'文件名',
+'File Type'=>'文件类型',
+'File Size'=>'大小',
+'LBL_INTERNAL'=>'内部',
+'LBL_URL'=>'URL',
+'LBL_EXTERNAL'=>'外部',
+'LBL_EXTERNAL_LNK'=>'外部链接',
+'LBL_ACTIVE'=>'活动',
+'LBL_INACTIVE'=>'非活动',
+'LBL_PLATFORM_INDEPENDENT'=>'平台无关',
+'LNK_DOWNLOAD'=>'下载',
+'LBL_MOVE_DOCUMENTS'=>'移动文档',
+'LBL_MOVE'=>'移动',
+'Download Type'=>'下载类型',
+'Version'=>'版本',
+'Active'=>'归档',
+'Download Count'=>'下载量',
+'Platform'=>'平台',
+'Assigned To'=>'指定给',
+// Module Sequence Numbering
+'Note No' => '文档序列号',
+// END
+
+// Documents module
+'LBL_EMPTY_FOLDERS' => '空目录',
+'LBL_VIEW_EMPTY_FOLDERS' => '查看空目录',
+'LBL_NO_DOCUMENTS' => '没有文档',
+'LBL_DESCRIPTION' => '描述',
+'FILE_HAS_NO_DATA'=>'选择的文件没有数据或者不是有效的文件',
+
+'LBL_MAXIMUM_20'=>'最大 20',
+'LBL_MAXIMUM_50'=>'最大 50',
+
+// Added after 5.1.0 GA
+'Folder Name'=>'文件夹名',
+'Document No'=>'文档序列号',
+);
+
+?>
