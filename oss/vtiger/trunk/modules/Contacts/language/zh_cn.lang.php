e79b344aa7ca6ca98739bce993b59780b705d27f
YuCheng Hu <yhu@isharkfly.com>
2010-11-16 02:05:03 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@587 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/Contacts/language/zh_cn.lang.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$mod_strings=Array(
+// Mike Crowe Mod --------------------------------------------------------Added for general search
+'LBL_MODULE_NAME'=>'联系人',
+'LBL_INVITEE'=>'针对报告',
+'LBL_MODULE_TITLE'=>'联系人：首页',
+'LBL_SEARCH_FORM_TITLE'=>'搜寻联系人',
+'LBL_LIST_FORM_TITLE'=>'联系人列表',
+'LBL_NEW_FORM_TITLE'=>'新增联系人',
+'LBL_CONTACT_OPP_FORM_TITLE'=>'联系人-机会：',
+'LBL_CONTACT'=>'联系人：',
+
+'LBL_LIST_NAME'=>'名称',
+'LBL_LIST_LAST_NAME'=>'姓名',
+'LBL_LIST_FIRST_NAME'=>'英文名',
+'LBL_LIST_CONTACT_NAME'=>'联系人名称',
+'LBL_LIST_TITLE'=>'在线IM',
+'LBL_LIST_ACCOUNT_NAME'=>'客户名称',
+'LBL_LIST_EMAIL_ADDRESS'=>'电子邮件',
+'LBL_LIST_PHONE'=>'电话',
+'LBL_LIST_CONTACT_ROLE'=>'职位',
+
+//DON'T CONVERT THESE THEY ARE MAPPINGS
+'db_last_name'=>'LBL_LIST_LAST_NAME',
+'db_first_name'=>'LBL_LIST_FIRST_NAME',
+'db_title'=>'LBL_LIST_TITLE',
+'db_email1'=>'LBL_LIST_EMAIL_ADDRESS',
+'db_email2'=>'LBL_LIST_EMAIL_ADDRESS',
+//END DON'T CONVERT
+	
+'LBL_EXISTING_CONTACT'=>'使用现有的联系人',
+'LBL_CREATED_CONTACT'=>'建立新的联系人',
+'LBL_EXISTING_ACCOUNT'=>'使用现有的客户',
+'LBL_CREATED_ACCOUNT'=>'建立新的客户',
+'LBL_CREATED_CALL'=>'建立新的电话记录l',
+'LBL_CREATED_MEETING'=>'建立新的会议',
+'LBL_ADDMORE_BUSINESSCARD'=>'增加其它的商业名片',
+
+'LBL_BUSINESSCARD'=>'商业名片',
+
+'LBL_NAME'=>'名称：',
+'LBL_CONTACT_NAME'=>'联系人名称：',
+'LBL_CONTACT_INFORMATION'=>'联系人信息',
+'LBL_CUSTOM_INFORMATION'=>'客户信息',
+'LBL_FIRST_NAME'=>'英文名：',
+'LBL_OFFICE_PHONE'=>'办公室电话：',
+'LBL_ACCOUNT_NAME'=>'客户名称：',
+'LBL_ANY_PHONE'=>'其它电话：',
+'LBL_PHONE'=>'电话：',
+'LBL_LAST_NAME'=>'姓名：',
+'LBL_MOBILE_PHONE'=>'手机：',
+'LBL_HOME_PHONE'=>'首页：',
+'LBL_LEAD_SOURCE'=>'接触来源：',
+'LBL_OTHER_PHONE'=>'其它电话：',
+'LBL_FAX_PHONE'=>'传真：',
+'LBL_TITLE'=>'在线IM：',
+'LBL_DEPARTMENT'=>'部门：',
+'LBL_BIRTHDATE'=>'生日：',
+'LBL_EMAIL_ADDRESS'=>'电子邮件：',
+'LBL_OTHER_EMAIL_ADDRESS'=>'其它电子邮件：',
+'LBL_ANY_EMAIL'=>'其它电子邮件：',
+'LBL_REPORTS_TO'=>'报告给：',
+'LBL_ASSISTANT'=>'助理：',
+'LBL_YAHOO_ID'=>'Yahoo账号：',
+'LBL_ASSISTANT_PHONE'=>'助理电话：',
+'LBL_DO_NOT_CALL'=>'请勿来电：',
+'LBL_EMAIL_OPT_OUT'=>'请勿寄电子邮件：',
+'LBL_PRIMARY_ADDRESS'=>'主要地址：',
+'LBL_ALTERNATE_ADDRESS'=>'其它地址：',
+'LBL_ANY_ADDRESS'=>'其它地址：',
+'LBL_CITY'=>'乡镇市区：',
+'LBL_STATE'=>'市（县）：',
+'LBL_POSTAL_CODE'=>'邮政编码：',
+'LBL_COUNTRY'=>'省（市/自治区）：',
+'LBL_DESCRIPTION_INFORMATION'=>'描述信息',
+'LBL_IMAGE_INFORMATION'=>'联系人照片：',
+'LBL_ADDRESS_INFORMATION'=>'地址信息',
+'LBL_DESCRIPTION'=>'描述：',
+'LBL_CONTACT_ROLE'=>'职位：',
+'LBL_OPP_NAME'=>'机会名称：',
+'LBL_DUPLICATE'=>'接触到潜在案件',
+'MSG_DUPLICATE'=>'建立这个联系人可能造成重复，您可以点选建立联系人来透过已存在的数据建立新的联系人或是点选取消。',
+
+'LNK_NEW_APPOINTMENT'=>'新增行程',
+'LBL_ADD_BUSINESSCARD'=>'新增商业名片',
+'NTC_DELETE_CONFIRMATION'=>'您确定要删除这笔记录？',
+'NTC_REMOVE_CONFIRMATION'=>'您确定要从这个案件中移除这位联系人？',
+'NTC_REMOVE_DIRECT_REPORT_CONFIRMATION'=>'您确定要将此记录从直接报告人处删除吗？',
+'ERR_DELETE_RECORD'=>'在删除联系人前必须输入正确的记录编号.',
+'NTC_COPY_PRIMARY_ADDRESS'=>'复制主要地址到替代地址',
+'NTC_COPY_ALTERNATE_ADDRESS'=>'复制替代地址到替代地址',
+
+'LBL_SELECT_CONTACT'=>'选择联系人',
+//Added for search heading
+'LBL_GENERAL_INFORMATION'=>'一般信息',
+
+
+
+//for v4 release added
+'LBL_NEW_POTENTIAL'=>'新增潜在',
+'LBL_POTENTIAL_TITLE'=>'潜在',
+
+'LBL_NEW_TASK'=>'新增任务',
+'LBL_TASK_TITLE'=>'任务',
+'LBL_NEW_CALL'=>'新增电话记录',
+'LBL_CALL_TITLE'=>'电话记录',
+'LBL_NEW_MEETING'=>'新增会议',
+'LBL_MEETING_TITLE'=>'会议',
+'LBL_NEW_EMAIL'=>'新增电子邮件',
+'LBL_EMAIL_TITLE'=>'电子邮件',
+'LBL_NEW_NOTE'=>'新增备忘录',
+'LBL_NOTE_TITLE'=>'备忘录',
+
+// Added for 4GA
+'LBL_TOOL_FORM_TITLE'=>'联系人编辑区',
+
+'Salutation'=>'问候',
+'First Name'=>'英文名',
+'Office Phone'=>'办公室电话',
+'Last Name'=>'姓名',
+'Mobile'=>'手机',
+'Account Name'=>'客户名称',
+'Home Phone'=>'家用电话',
+'Lead Source'=>'客户来源',
+'Other Phone'=>'电话',
+'Title'=>'在线QQ',
+'Fax'=>'传真',
+'Department'=>'部门',
+'Birthdate'=>'生日',
+'Email'=>'电子邮件',
+'Reports To'=>'报告给',
+'Assistant'=>'助理',
+'Yahoo Id'=>'Yahoo账号',
+'Assistant Phone'=>'助理电话',
+'Do Not Call'=>'拒绝来电',
+'Email Opt Out'=>'拒绝电子邮件',
+'Assigned To'=>'负责人',
+'Campaign Source'=>'活动来源',
+'Reference'=>'参考',
+'Created Time'=>'建立时间',
+'Modified Time'=>'修改时间',
+'Mailing Street'=>'邮寄地址',
+'Other Street'=>'其它地址',
+'Mailing City'=>'乡镇市区',
+'Mailing State'=>'市（县）',
+'Mailing Zip'=>'邮政编码',
+'Mailing Country'=>'省（市/自治区）',
+'Mailing Po Box'=>'采购订单信箱',
+'Other Po Box'=>'其它采购订单信箱',
+'Other City'=>'乡镇市区',
+'Other State'=>'市（县）',
+'Other Zip'=>'邮政编码',
+'Other Country'=>'省（市/自治区）',
+'Contact Image'=>'照片',
+'Description'=>'描述',
+
+// Added vtiger_fields for Add Business Card
+'LBL_NEW_CONTACT'=>'新增联系人',
+'LBL_NEW_ACCOUNT'=>'新增客户',
+'LBL_NOTE_SUBJECT'=>'备忘录标题：',
+'LBL_NOTE'=>'备忘录：',
+'LBL_WEBSITE'=>'网站：',
+'LBL_NEW_APPOINTMENT'=>'新增行程',
+'LBL_SUBJECT'=>'在线IM：',
+'LBL_START_DATE'=>'开始日期：',
+'LBL_START_TIME'=>'开始时间：',
+
+//Added vtiger_field after 4_0_1
+'Portal User'=>'网站账号',
+'LBL_CUSTOMER_PORTAL_INFORMATION'=>'网站客户信息',
+'Support Start Date'=>'支持开始日期',
+'Support End Date'=>'支持结束日期',
+//Added for 4.2 Release -- CustomView
+'Name'=>'名称',
+'LBL_ALL'=>'全部',
+'LBL_MAXIMUM_LIMIT_ERROR'=>'抱歉，文件超过系统限制，请保持上传文件在800000bytes以下',
+'LBL_UPLOAD_ERROR'=>'上传失败，请重试！',
+'LBL_IMAGE_ERROR'=>'提供的文件不是图片格式(.gif/.jpg/.png)',
+'LBL_INVALID_IMAGE'=>'文件有误或是没有数据',
+
+//Added after 5Alpha5
+'Notify Owner'=>'提醒负责人',
+
+//Added for Picklist Values
+'--None--'=>'--None--',
+
+'Mr.'=>'先生',
+'Ms.'=>'小姐',
+'Mrs.'=>'太太',
+'Dr.'=>'博士',
+'Prof.'=>'专家',
+
+'Cold Call'=>'陌生拜访',
+'Existing Customer'=>'既有客户',
+'Self Generated'=>'自动产生',
+'Employee'=>'员工',
+'Partner'=>'伙伴',
+'Public Relations'=>'公众关系',
+'Direct Mail'=>'邮件营销',
+'Conference'=>'研讨会',
+'Trade Show'=>'贸易展览',
+'Web Site'=>'网站',
+'Word of mouth'=>'口耳相传',
+'Other'=>'其它',
+'User List'=>'使用者清单',
+
+//Added for 5.0.3
+'Customer Portal Login Details'=>'顾客注册细节',
+'Dear'=>'亲爱的',
+'Your Customer Portal Login details are given below:'=>'您的顾客提供了以下注册细节:',
+'User Id :'=>'用户号:',
+'Password :'=>'密码:',
+'Please Login Here'=>'请在这里登录',
+'Note :'=>'笔记:',
+'We suggest you to change your password after logging in first time'=>'请您在第一次登录时修改密码以保证安全.',
+'Support Team'=>'项目团队',
+
+'TITLE_AJAX_CSS_POPUP_CHAT'=>'Ajax Css-Popup chat',
+
+// Added after 5.0.4 GA
+
+// Module Sequence Numbering
+'Contact Id'=>'编号',
+//END
+
+);
+
+?>
