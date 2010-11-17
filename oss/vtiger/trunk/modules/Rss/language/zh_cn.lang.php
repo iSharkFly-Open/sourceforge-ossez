e0a7fe27e34b7205a7b94ecb30eacdccf03f3d02
YuCheng Hu <yhu@isharkfly.com>
2010-11-17 02:01:32 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@640 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/Rss/language/zh_cn.lang.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$mod_strings = Array(
+'LBL_MODULE_NAME' => '新闻',
+'LBL_MODULE_TITLE' => '新闻：首页',
+
+'LBL_NEW_RSS_ENTRY' => '新闻RSS资料来源网址：',
+'LBL_SEARCH_RSS' => '标题搜寻：',
+'LBL_NEW_RSS_BUTTON' => '新增',
+
+'LBL_STARRED_RSS' => '重要新闻',
+'LBL_POP_CRM_RSS' => 'CRM相关热门新闻',
+'LBL_ALL_RSS_FEEDS' => '所有他站新闻',
+'LBL_VTIGER_RSS_READER'=>'vtiger RSS 读者',
+
+'LBL_MORE' => '更多...',
+'LBL_SUBJECT' => '主旨',
+'LBL_SENDER' => '寄件人',
+'LBL_CATEGORY' => '类别：',
+'LBL_FEED' => '资料来源：',
+'LBL_FEED_SOURCES'=> 'Feed 来源',
+'LBL_ADD_RSS_FEED' => '新增 RSS 资料来源',
+'LBL_DELETE_BUTTON'=>'删除',
+'LBL_SET_DEFAULT_BUTTON'=>'预设',
+'LBL_FEEDS_LIST' => '资料来源：',
+'LBL_ERROR_MSG' => '没有选择 RSS 来源或是您的代理服务器设定有问题',
+'LBL_REGRET_MSG' => '抱歉：无法取得 RSS 网址',
+
+'UNABLE_TO_SAVE'=>'无法保存 RSS 资料地址',
+
+// Added/Updated for vtiger CRM 5.0.4
+'NOT_A_VALID'=>'不是一个有效的rss资料或你的代理服务器设置是不正确的',
+
+// Added after 5.0.4 GA
+'INVALID_RSS_URL' => 'Not a valid RSS URL',
+'UNABLE_TO_SAVE' => 'Unable to save the Url',
+);
+?>
