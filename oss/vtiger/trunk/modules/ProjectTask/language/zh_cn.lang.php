7eeb27907a8ba52dced848a6209a323046aad978
YuCheng Hu <yhu@isharkfly.com>
2010-11-17 01:36:04 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@635 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/ProjectTask/language/zh_cn.lang.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$mod_strings = Array(
+
+    'LBL_MODULE_NAME'=>'Project Tasks',
+    'LBL_MODULE_TITLE'=>'Project Tasks',
+    'LBL_NEW_PROJECTTASK'=>'New Project Task',
+    'LBL_PROJECTTASKS'=>'Project Tasks',
+    'LBL_PROJECTTASK'=>'Project Task',
+    'Project Task ID'=>'Project Task ID',
+    'projecttasks' => 'projecttasks',
+    //added this to translate the module name in the main app menu
+    'ProjectTask'=>'Project Tasks',
+    'SINGLE_ProjectTask' => 'Project Task',
+    'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+    'LBL_PROJECT_TASK_INFORMATION' => 'Project Task Information',
+    'LBL_DESCRIPTION_INFORMATION' => 'Description Information',
+    'Assigned To' => 'Assigned To',
+    //'linktoproject' => 'Link to Project',
+    'Related Project' => 'Related Project',
+    'Priority' => 'Priority',
+    'Progress' => 'Progress',
+    'Type'=>'Type',
+    'Worked Hours'=>'Worked Hours',
+    'Start Date'=>'Start Date',
+    'End Date'=>'End Date',
+    'Related To' => 'Related To',
+    'projectid' => 'Related To',
+    'administrative' => 'administrative',
+    'operative' => 'operative',
+    'other' => 'other',
+    'low' => 'low',
+    'normal' => 'normal',
+    'high' => 'high',
+    'Created Time' => 'Created Time',
+    'Modified Time' => 'Modified Time',
+    'Description' => 'Description',
+    'description' => 'Description',
+    //added for related lists 
+    'Project Task Name' => 'Task Name',
+    'Project Task No' => 'Task No',
+    'Project Task Number' => 'Task Code',
+);
+
+?>
