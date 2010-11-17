4205287c0334cc064d17ee246704dc6edc818d65
YuCheng Hu <yhu@isharkfly.com>
2010-11-17 01:27:24 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@634 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/ProjectMilestone/language/zh_cn.lang.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$mod_strings = Array(
+	 
+	'LBL_MODULE_NAME'=>'Project Milestones',
+  	'LBL_MODULE_TITLE'=>'Project Milestones',
+	'LBL_NEW_OPERATION'=>'New Project Milestone',
+    'LBL_MILESTONES'=>'Project Milestones',
+    'LBL_MILESTONE'=>'Project Milestone',
+    'Project Milestone ID'=>'Milestone ID', 
+    'projectmilestones' => 'Project Milestones',
+    //added this to translate the module name in the main app menu
+    'ProjectMilestone'=>'Project Milestones',
+    //
+    'SINGLE_ProjectMilestone' => 'Project Milestone',
+    'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+    'LBL_PROJECT_MILESTONE_INFORMATION' => 'Project Milestone Information',
+    'LBL_DESCRIPTION_INFORMATION' => 'Description Information',
+    'Assigned To' => 'Assigned To',
+    'Milestone Date' => 'Milestone Date',
+    'Milestone' => 'Milestone',
+    'Type'=>'Type',
+    //'linktoproject'=>'Link To',
+    'Related Project' => 'Related Project',
+    'Related To' => 'Related To',
+    'projectid' => 'Related To',
+  	'administrative' => 'administrative',
+    'operative' => 'operative',
+    'other' => 'other',
+    'Created Time' => 'Created Time',
+    'Modified Time' => 'Modified Time',
+    'description' => 'Description',
+    'Description' => 'Description',
+    //added for related lists 
+    'Project Milestone Name' => 'Milestone Name',
+    'Project Milestone No' => 'Milestone No',
+
+);
+
+?>
