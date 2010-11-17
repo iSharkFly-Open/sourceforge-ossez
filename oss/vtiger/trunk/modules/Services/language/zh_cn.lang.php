7466fd6da13c44bae540f68774a730a32a5fd0d6
YuCheng Hu <yhu@isharkfly.com>
2010-11-17 02:05:15 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@643 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/Services/language/zh_cn.lang.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$mod_strings = Array(
+'LBL_MODULE_NAME'=>'服务',
+'SINGLE_Services'=>'服务',
+'LBL_LIST_SERVICE_NAME'=>'服务名称',
+'LBL_PRICEBOOK'=>'价格表:',
+'LBL_PRICEBOOK_INFORMATION'=>'价格表信息:',
+'LBL_PRICING_INFORMATION'=>'价格信息:',
+'LBL_LIST_FORM_TITLE'=>'服务列表',
+'LBL_LIST_PRICEBOOK_FORM_TITLE'=>'价格列表',
+'LBL_SERVICE_UNIT_PRICE'=>'单价',
+'LBL_PB_LIST_PRICE'=>'List Price',
+'LBL_SERVICE_INFORMATION'=>'服务信息',
+'LBL_CUSTOM_INFORMATION'=>'Custom Information',
+'LBL_DESCRIPTION_INFORMATION'=>'详细信息',
+
+'LBL_ADD_PRICEBOOK_BUTTON_LABEL'=>'增加到价格表',
+
+'Services'=>'服务',
+'Service Name'=>'服务名称',
+'Service Active'=>'启用',
+'Service Category'=>'分类',
+'Service No'=>'服务编号',
+'Owner'=>'负责人',
+'Days'=>'天数',
+'Hours'=>'小时',
+'Incidents'=>'事件',
+'No of Units'=>'No of Units',
+'Commission Rate'=>'佣金比例 (%)',
+'Unit Price'=>'单价',
+'Price'=>'价格',
+'Support Start Date'=>'支持开始日期',
+'Support Expiry Date'=>'支持结束日期',
+'Sales Start Date'=>'销售开始日期',
+'Sales End Date'=>'销售结束日期',
+'Created Time'=>'创建时间',
+'Modified Time'=>'修改时间',
+'Related To'=>'关联到',
+'Usage Unit'=>'Usage Unit',
+'Currency'=>'币种',
+'Tax Class'=>'Tax Class',
+'Website'=>'网站',
+'Description'=>'描述',
+
+'Price Book Name'=>'价格表名称',
+
+'LBL_NO_TAXES_ASSOCIATED'=>'No taxes associated with this service',
+'--None--'=>'--None--',
+'Installation'=>'Installation',
+'Migration'=>'Migration',
+'Customization'=>'Customization',
+'Training'=>'Training',
+);
+
+?>
