479df644a8b6a8f11bd954c4d26cad6153472538
YuCheng Hu <yhu@isharkfly.com>
2010-11-17 01:20:29 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@631 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/PriceBooks/language/zh_cn.lang.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$mod_strings = array(
+
+'LBL_PRICEBOOK_INFORMATION' => '价目表信息：',
+'LBL_CUSTOM_INFORMATION' => '自订信息：',
+'LBL_DESCRIPTION_INFORMATION' => '描述名称：',
+//Mapping for price book
+'Price Book Name' => '价目表名称',
+'Product Name' => '产品名称',
+'Active' => '启用',
+'Description' => '说明',
+'Created Time' => '建立时间',
+'Modified Time' => '修改时间',
+'LBL_LIST_PRODUCT_NAME' => '产品名称',
+'LBL_PRODUCT_CODE' => '产品代号',
+'LBL_PRODUCT_UNIT_PRICE' => '单位价格',
+'LBL_PB_LIST_PRICE' => '定价',
+'LBL_ACTION' => '操作',
+'PriceBook' => '价目表',
+// Added after 5.0.4 GA
+'Currency'=>'Currency',
+
+// Module Sequence Numbering
+'PriceBook No' => 'PriceBook No',
+// END
+);
+
+
+
+?>
