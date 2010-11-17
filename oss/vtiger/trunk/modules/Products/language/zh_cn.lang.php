4d3c2e1c9cf2a47b4158b41e9be2241b312d8077
YuCheng Hu <yhu@isharkfly.com>
2010-11-17 01:21:48 +0000

    更新到 5.2 正式版后第一次 SVN 提交。
    yuchenghu@hawebs.net
    
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@632 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/Products/language/zh_cn.lang.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * Copyright (C) 2006-2010 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * https://www.hawebs.org/forums/computer/
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ * 
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ *
+ * ---------------------------------------------
+ */
+$mod_strings = Array(
+'LBL_MODULE_NAME' => '产品',
+'SINGLE_Products'=>'产品',
+'LBL_LIST_PRODUCT_NAME' => '产品名称',
+'LBL_TOOL_FORM_TITLE' => '产品工具',
+'LBL_SEARCH_FORM_TITLE' => '搜寻产品',
+'LBL_PRODUCT_NAME' => '产品类型名称：',
+'LBL_VENDOR_NAME' => '厂商名称：',
+'LBL_PRICEBOOK_NAME' => '价目表名称：',
+'LBL_VENDOR' => '厂商',
+'LBL_PRICEBOOK' => '价目表：',
+'LBL_VENDOR_INFORMATION' => '厂商信息：',
+'LBL_PRICEBOOK_INFORMATION' => '价目表信息：',
+'LBL_VENDOR_ADDRESS_INFORMATION' => '地址信息：',
+'LBL_DESCRIPTION_INFORMATION' => '描述信息',
+'LBL_PRICING_INFORMATION' => '定价信息：',
+'LBL_STOCK_INFORMATION' => '存货信息：',
+'LBL_LIST_FORM_TITLE' => '产品列表',
+'LBL_LIST_VENDOR_FORM_TITLE' => '厂商列表',
+'LBL_LIST_PRICEBOOK_FORM_TITLE' => '价目表清单',
+'LBL_LIST_CATEGORY' => '产品分类',
+'LBL_CATEGORY' => '产品分类：',
+'LBL_PRODUCT_CODE' => '产品编码',
+'LBL_LIST_PRODUCT_CODE' => '产品类型编码：',
+'LBL_PRODUCT_ACTIVE' => '产品任务',
+'LBL_PRODUCT_COMMISSION_RATE' => '评价',
+'LBL_PRODUCT_QTY_PER_UNIT' => '量/单位',
+'LBL_PRODUCT_UNIT_PRICE' => '单位价格',
+'LBL_PB_LIST_PRICE' => '定价',
+'LBL_PRODUCT_DESCRIPTION' => '描述',
+'LBL_PRODUCT_IMAGE' => '图片',
+'LBL_NEW_FORM_TITLE' => '新增产品',
+'LBL_PRODUCT_INFORMATION' => '产品信息',
+'LBL_CUSTOM_INFORMATION' => '自订信息',
+'LBL_DESCRIPTION_INFORMATION'=>'描述信息',
+
+//New entry
+'LBL_UPLOAD'=>'上传文件',
+
+'LBL_PRODUCT' => '产品类型',
+'LBL_NEW_PRODUCT' => '新增产品类型',
+'LBL_COLON'=>':',
+
+'LBL_ATTACHMENTS' => '附件',
+'LBL_ATTACH_FILE' => '附件档案',
+'LBL_DESCRIPTION'=>'描述',
+'LBL_FILE'=>'文件名称',
+
+
+//for v4 release added
+'LBL_NEW_POTENTIAL' => '新增潜在机会',
+'LBL_POTENTIAL_TITLE' => '潜在机会',
+
+'LBL_NEW_TICKET' => '新增记事签',
+'LBL_TICKET_TITLE' => '记事签',
+
+'LBL_NEW_CALL' => '新增电话记录',
+'LBL_CALL_TITLE' => '电话记录',
+
+'LBL_NEW_MEETING' => '新增会议',
+'LBL_MEETING_TITLE' => '会议',
+
+'LBL_NEW_ATTACHMENT'=>'新附件',
+'LBL_ATTACHMENT_TITLE'=>'附件',
+
+//Added vtiger_fields after RC1 - Release
+'LBL_MANUFACTURER' => '厂商',
+'LBL_PRODUCTCATEGORY' => '产品分类',
+'LBL_SUPPORT_START_DATE' => '上架日期',
+'LBL_SUPPORT_EXPIRY_DATE' => '下架日期',
+'LBL_PURCHASE_DATE' => '购买日期',
+
+//Added for 4GA
+'Product Name' => '产品名称',
+//'Product Code'=>'Part Number',
+'Part Number' => '部件编码',
+'Product Active' => '产品任务',
+'Commission Rate' => '评价 (%)',
+'Qty/Unit' => '量/单位',
+'Unit Price' => '单位价格',
+'Manufacturer' => '厂商',
+'Product Category' => '产品分类',
+'Support Start Date' => '上架日期',
+'Support Expiry Date' => '下架日期',
+'Purchase Date' => '购买日期',
+'Sales Start Date' => '开始销售日期',
+'Sales End Date' => '终止销售日期',
+'Created Time' => '建立时间',
+'Modified Time' => '修改时间',
+'Related To' => '相关人事',
+'Usage Unit' => '使用单位',
+'Handler' => '处理者',
+'Contact Name' => '联络人名称',
+'Currency' => '货币',
+'Reorder Level' => '记录者层级',
+'Tax Class' => '税别',
+'Mfr PartNo' => '零件编号',
+'Vendor PartNo' => '厂商零件编号',
+'Serial No' => '序号',
+'Qty In Stock' => '存货数量',
+'Product Sheet' => '产品表',
+'Qty In Demand' => '需求数量',
+'GL Account' => '会计科目',
+'Vendor Name' => '厂商名称',
+
+//Mapping for Name
+'Name' => '名称',
+'Company Name' => '公司名称',
+'Phone' => '电话',
+'Email' => '电子信箱',
+'Website'=>'网站',
+'GL Account'=>'企业类型',
+'Category' => '类别',
+'Street' => '街道巷弄',
+'City' => '乡镇市区',
+'State' => '县市',
+'Postal Code'=>'邮政编码',
+'Country'=>'国家',
+'Description'=>'描述',
+'Po Box'=>'采购订单信箱',
+
+//Mapping for price book
+'Price Book Name' => '价目表名称',
+'Product Name'=>'产品名称',
+'Active'=>'启用',
+
+//Added for 4.2 Release -- CustomView
+'LBL_ALL' => '全部',
+'LBL_VENDOR_SEARCH_FORM_TITLE' => '搜寻厂商',
+'LBL_PRICEBOOK_SEARCH_FORM_TITLE' => '搜寻价目表',
+'LBL_VENDOR_TITLE' => '厂商',
+'LBL_PRICEBOOK_TITLE' => '价目表',
+'LBL_PRODUCTS_TITLE' => '产品',
+'LBL_COMPANY_NAME' => '公司名称',
+'LBL_CATEGORY_TITLE' => '类别',
+'LBL_ACTIVE_TITLE' => '启用',
+'LBL_LIST_VENDOR_NAME' => '厂商名称',
+'LBL_IMAGE_INFORMATION' => '业务文件信息：',
+'Product Image' => '业务文件',
+'LBL_ADD_PRICEBOOK_BUTTON_TITLE' => '新增到价目表 [Alt+B]',
+'LBL_ADD_PRICEBOOK_BUTTON_KEY' => 'B',
+'LBL_ADD_PRICEBOOK_BUTTON_LABEL' => '新增到价目表',
+'LBL_ADD_PRODUCT_PRICEBOOKS' => '新增产品到价目表',
+'LBL_ADD_PRODUCTS_PRICEBOOK' => '新增产品到价目表',
+
+//Added for 4.2 patch2 release
+'LBL_MAXIMUM_LIMIT_ERROR' => '抱歉，上传的档案大小超过限制，请将档案控制在小于 800 KB',
+'LBL_UPLOAD_ERROR' => '档案上传时发生错误，请重试！',
+'LBL_IMAGE_ERROR' => '提供的档案并非图片格式(.gif/.jpg/.png)',
+'LBL_INVALID_IMAGE' => '档案错误或是一个空的档案',
+'LBL_EMAIL'=>'电子邮件',
+'Product'=>'产品',
+'LBL_EDITLISTPRICE'=>'编辑定价',
+
+//Added for existing picklist entries
+
+'--None--'=>'--无--',
+
+'Hardware'=>'其他',
+'Software'=>'签证',
+'CRM Applications'=>'企业注册',
+
+'300-Sales-Software' => '投资性公司',
+'301-Sales-Hardware' => '股份有限公司',
+'302-Rental-Income' => '研发机构',
+'303-Interest-Income' => '拍卖企业',
+'304-Sales-Software-Support' => '物流',
+'305-Sales Other' => '贸易公司',
+'306-Internet Sales' => '资产评估机构',
+'307-Service-Hardware Labor' => '人才中介机构',
+'308-Sales-Books' => '其他',
+
+'Box' => '盒',
+'Carton' => '纸板',
+'Caton' => 'Caton',
+'Dozen' => '打',
+'Each' => '每件',
+'Hours' => '小时',
+'Impressions' => '印象',
+'Lb' => '磅',
+'M' => '公尺',
+'Pack' => '包装',
+'Pages' => '包装',
+'Pieces' => '片',
+'Quantity' => '数量',
+'Reams' => '大量',
+'Sheet' => '张',
+'Spiral Binder' => '打包带',
+'Sq Ft' => '平方英呎',
+
+//Added for 5.0.3
+'MSG_STOCK_LEVEL'=>'库存水平依然很低',
+'MSG_DEAR'=>'Dear',
+'MSG_CURRENT_STOCK'=>'目前存量',
+'MSG_IN_OUR_WAREHOUSE'=>'在我们的仓库',
+'MSG_PROCURE_REQUIRED_NUMBER'=>'采购所需数量的单位作为存货水平低于订货点水平',
+'MSG_SEVERITY'=>'严重性：重大',
+'MSG_THANKS'=>'Thanks,',
+
+);
+
+?>
