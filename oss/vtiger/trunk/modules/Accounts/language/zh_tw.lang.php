b057877e658be068bb87bd40b91c7592310edbe9
YuCheng Hu <yhu@isharkfly.com>
2010-12-20 06:04:49 +0000

    更新模块 Accounts 的繁体中文语言文件。
    yuchenghu@hawebs.net
    
    http://www.hawebs.net
    
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@658 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

--- oss/vtiger/trunk/modules/Accounts/language/zh_tw.lang.php
+++ oss/vtiger/trunk/modules/Accounts/language/zh_tw.lang.php
@@ -21,75 +21,75 @@
  * ---------------------------------------------
  */
 $mod_strings=Array(
-'LBL_MODULE_NAME'=>'客戶',
-'LBL_MODULE_TITLE'=>'客戶：首頁',
-'LBL_SEARCH_FORM_TITLE'=>'搜尋客戶',
-'LBL_LIST_FORM_TITLE'=>'客戶列表',
-'LBL_NEW_FORM_TITLE'=>'新增客戶',
-'LBL_MEMBER_ORG_FORM_TITLE'=>'成員組織性質',
-//LabelforTopAccountsinHomePage,addedfor4.2GA
-'LBL_TOP_ACCOUNTS'=>'我的重要客戶',
+'LBL_MODULE_NAME'=>'帳戶',
+'LBL_MODULE_TITLE'=>'帳戶：首頁',
+'LBL_SEARCH_FORM_TITLE'=>'搜尋帳戶',
+'LBL_LIST_FORM_TITLE'=>'帳戶列表',
+'LBL_NEW_FORM_TITLE'=>'新增帳戶',
+'LBL_MEMBER_ORG_FORM_TITLE'=>'成員組織',
+// Label for Top Accounts in Home Page, added for 4.2 GA
+'LBL_TOP_ACCOUNTS'=>'我的重要帳戶',
 'LBL_TOP_AMOUNT'=>'金額',
-'LBL_LIST_ACCOUNT_NAME'=>'客戶名稱',
-'LBL_LIST_CITY'=>'鄉鎮市區',
+'LBL_LIST_ACCOUNT_NAME'=>'帳戶名稱',
+'LBL_LIST_CITY'=>'城市',
 'LBL_LIST_WEBSITE'=>'網站',
-'LBL_LIST_STATE'=>'縣市',
+'LBL_LIST_STATE'=>'州',
 'LBL_LIST_PHONE'=>'電話',
 'LBL_LIST_EMAIL_ADDRESS'=>'電子郵件地址',
 'LBL_LIST_CONTACT_NAME'=>'聯系人姓名',
-'LBL_LIST_AMOUNT'=>'機會總計',
+'LBL_LIST_AMOUNT'=>'合共總計',
 
-//DON'TCONVERTTHESETHEYAREMAPPINGS
+//DON'T CONVERT THESE THEY ARE MAPPINGS
 'db_name'=>'LBL_LIST_ACCOUNT_NAME',
 'db_website'=>'LBL_LIST_WEBSITE',
 'db_billing_address_city'=>'LBL_LIST_CITY',
 
-//ENDDON'TCONVERT
+//END DON'T CONVERT
 
 'LBL_ACCOUNT'=>'客戶：',
 'LBL_ACCOUNT_NAME'=>'客戶名稱：',
 'LBL_PHONE'=>'電話：',
 'LBL_WEBSITE'=>'網站：',
 'LBL_FAX'=>'傳真：',
-'LBL_TICKER_SYMBOL'=>'傳票符號：',
+'LBL_TICKER_SYMBOL'=>'票符號：',
 'LBL_OTHER_PHONE'=>'其它電話：',
-'LBL_ANY_PHONE'=>'其它電話：',
+'LBL_ANY_PHONE'=>'任何電話：',
 'LBL_MEMBER_OF'=>'成員：',
 'LBL_EMAIL'=>'電子郵件：',
 'LBL_EMPLOYEES'=>'員工：',
 'LBL_OTHER_EMAIL_ADDRESS'=>'其它電子郵件：',
-'LBL_ANY_EMAIL'=>'其它電子郵件：',
+'LBL_ANY_EMAIL'=>'任何電子郵件：',
 'LBL_OWNERSHIP'=>'所有者：',
 'LBL_RATING'=>'評價：',
-'LBL_INDUSTRY'=>'行業別：',
+'LBL_INDUSTRY'=>'行業：',
 'LBL_SIC_CODE'=>'統一編號：',
 'LBL_TYPE'=>'類型：',
-'LBL_ANNUAL_REVENUE'=>'年營業額：',
-'LBL_ADDRESS_INFORMATION'=>'地址信息',
-'LBL_ACCOUNT_INFORMATION'=>'賬號信息',
-'LBL_CUSTOM_INFORMATION'=>'客戶信息',
+'LBL_ANNUAL_REVENUE'=>'全年收入：',
+'LBL_ADDRESS_INFORMATION'=>'地址資料',
+'LBL_ACCOUNT_INFORMATION'=>'賬號資料',
+'LBL_CUSTOM_INFORMATION'=>'客戶資料',
 'LBL_BILLING_ADDRESS'=>'賬單地址：',
 'LBL_SHIPPING_ADDRESS'=>'送貨地址：',
 'LBL_ANY_ADDRESS'=>'其它地址：',
-'LBL_CITY'=>'鄉鎮市區：',
-'LBL_STATE'=>'縣市：',
+'LBL_CITY'=>'城市：',
+'LBL_STATE'=>'州：',
 'LBL_POSTAL_CODE'=>'郵政編碼：',
 'LBL_COUNTRY'=>'國家：',
-'LBL_DESCRIPTION_INFORMATION'=>'描述信息',
+'LBL_DESCRIPTION_INFORMATION'=>'描述資料',
 'LBL_DESCRIPTION'=>'描述：',
 'NTC_COPY_BILLING_ADDRESS'=>'復制賬單地址內容到送貨地址',
 'NTC_COPY_SHIPPING_ADDRESS'=>'復制送貨地址內容到賬單地址',
 'NTC_REMOVE_MEMBER_ORG_CONFIRMATION'=>'您確定要刪除這筆成員/組織的記錄嗎？',
-'LBL_DUPLICATE'=>'復制潛在案件客戶',
-'MSG_DUPLICATE'=>'由潛在案件客戶中復制建立新客戶，您可以點選客戶列表中的客戶以復制建立新的客戶，并且繼承它的相關數據。',
+'LBL_DUPLICATE'=>'復制潛在客戶',
+'MSG_DUPLICATE'=>'由潛在客戶中復制建立新客戶，您可以點選客戶列表中的客戶以復制建立新的客戶，并且繼承它的相關數據。',
 
-'LBL_INVITEE'=>'聯系人',
+'LBL_INVITEE'=>'聯絡人',
 'ERR_DELETE_RECORD'=>'要刪除賬號您必須指定一筆記錄編號.',
 
-'LBL_SELECT_ACCOUNT'=>'選擇客戶',
+'LBL_SELECT_ACCOUNT'=>'選擇帳戶',
 'LBL_GENERAL_INFORMATION'=>'主要信息',
 
-//forv4releaseadded
+//for v4 release added
 'LBL_NEW_POTENTIAL'=>'新增潛在案件',
 'LBL_POTENTIAL_TITLE'=>'潛在案件',
 
@@ -101,20 +101,20 @@ $mod_strings=Array(
 'LBL_MEETING_TITLE'=>'會議',
 'LBL_NEW_EMAIL'=>'新增電子郵件',
 'LBL_EMAIL_TITLE'=>'郵件',
-'LBL_NEW_CONTACT'=>'新增聯系人',
-'LBL_CONTACT_TITLE'=>'聯系人',
+'LBL_NEW_CONTACT'=>'新增聯絡人',
+'LBL_CONTACT_TITLE'=>'聯絡人',
 
-//Addedvtiger_fieldsafterRC1-Release
+//Added vtiger_fields after RC1 - Release
 'LBL_ALL'=>'全部',
-'LBL_PROSPECT'=>'期望',
+'LBL_PROSPECT'=>'前景',
 'LBL_INVESTOR'=>'投資者',
-'LBL_RESELLER'=>'轉售人',
-'LBL_PARTNER'=>'伙伴',
+'LBL_RESELLER'=>'經銷商',
+'LBL_PARTNER'=>'合作夥伴',
 
+// Added for 4GA
+'LBL_TOOL_FORM_TITLE'=>'帳戶工具',
 //Addedfor4GA
-'LBL_TOOL_FORM_TITLE'=>'客戶工具',
-//Addedfor4GA
-'AccountName'=>'客戶名稱',
+'AccountName'=>'帳戶名稱',
 'Phone'=>'電話',
 'Website'=>'網址',
 'Fax'=>'傳真',
@@ -129,70 +129,70 @@ $mod_strings=Array(
 'industry'=>'行業別',
 'SICCode'=>'統一編號',
 'Type'=>'類型',
-'AnnualRevenue'=>'年營業額',
+'AnnualRevenue'=>'全年收入',
 'AssignedTo'=>'負責人',
 'BillingAddress'=>'賬單地址',
 'ShippingAddress'=>'送貨地址',
-'BillingCity'=>'鄉鎮市區',
-'ShippingCity'=>'鄉鎮市區',
-'BillingState'=>'縣市',
-'ShippingState'=>'縣市',
-'BillingCode'=>'郵政編碼',
-'ShippingCode'=>'郵政編碼',
-'BillingCountry'=>'國家',
-'ShippingCountry'=>'國家',
+'BillingCity'=>'城市地址',
+'ShippingCity'=>'送貨城市',
+'BillingState'=>'州地址',
+'ShippingState'=>'送貨州',
+'BillingCode'=>'郵政編碼地址',
+'ShippingCode'=>'送貨郵政編碼',
+'BillingCountry'=>'國家'地址,
+'ShippingCountry'=>'送貨國家',
 'CreatedTime'=>'建立時間',
 'ModifiedTime'=>'修改時間',
 'Description'=>'描述',
 'ShippingPoBox'=>'付款地址采購訂單信箱',
 'BillingPoBox'=>'付款地址采購訂單信箱',
 
-//Addedafter4.2patch2
+//Added after 4.2 patch 2
 'EmailOptOut'=>'拒絕電子郵件打擾',
 'LBL_EMAIL_OPT_OUT'=>'拒絕電子郵件打擾：',
 
-//Addedafter5Alpha5
+//Added after 5Alpha5
 'NotifyOwner'=>'提醒負責人',
 
-//Addedforexistingpicklistentries
+//Added for existing picklist entries
 
 '--None--'=>'--無--',
 
 'Acquired'=>'取得',
 'Active'=>'啟用',
-'MarketFailed'=>'市場失利',
+'MarketFailed'=>'推銷失敗',
 'ProjectCancelled'=>'項目取消',
 'Shutdown'=>'關閉',
 
-'Apparel'=>'存在',
+'Apparel'=>'服裝',
 'Banking'=>'銀行',
 'Biotechnology'=>'生物科技',
 'Chemicals'=>'化學',
-'Communications'=>'交通',
-'Construction'=>'建筑',
+'Communications'=>'交通業',
+'Construction'=>'建築業',
 'Consulting'=>'顧問',
-'Education'=>'教育',
-'Electronics'=>'電子',
+'Education'=>'教育業',
+'Electronics'=>'電子業',
 'Energy'=>'能源',
-'Engineering'=>'工程',
-'Entertainment'=>'娛樂',
-'Environmental'=>'環境',
+'Engineering'=>'工程業',
+'Entertainment'=>'娛樂事業',
+'Environmental'=>'環境業',
 'Finance'=>'財務',
 'Food&Beverage'=>'飲食',
 'Government'=>'政府',
-'Healthcare'=>'健康照護',
+'Healthcare'=>'醫療保健',
 'Hospitality'=>'醫院',
-'Insurance'=>'保險',
-'Machinery'=>'機械',
-'Manufacturing'=>'工廠',
-'Media'=>'媒體',
-'NotForProfit'=>'非營利',
-'Recreation'=>'娛樂中心',
-'Retail'=>'零售',
-'Shipping'=>'運輸',
-'Technology'=>'科技',
-'Telecommunications'=>'通訊',
-'Transportation'=>'傳輸',
+'Insurance'=>'保險業',
+'Machinery'=>'機械業',
+'Manufacturing'=>'製造業',
+'Media'=>'媒體行業',
+'Not For Profit'=>'非營利機構',
+'Recreation'=>'康樂',
+'Retail'=>'零售業',
+'Shipping'=>'運輸業',
+'Technology'=>'資訊科技',
+'Telecommunications'=>'通訊業',
+'Transportation'=>'交通業',
 'Utilities'=>'工具',
 'Other'=>'其它',
 
@@ -204,38 +204,37 @@ $mod_strings=Array(
 'Partner'=>'伙伴',
 'Press'=>'新聞',
 'Prospect'=>'潛在客戶',
-'Reseller'=>'盤商',
+'Reseller'=>'經銷商',
 'LBL_START_DATE'=>'開始日期',
 'LBL_END_DATE'=>'結束日期',
 
-//Added/UpdatedforvtigerCRM5.0.4
+// Added/Updated for vtiger CRM 5.0.4
 
-//addedtofixtheissue#4081
+//added to fix the issue #4081
 'LBL_ACCOUNT_EXIST'=>'帳戶名稱已存在!',
 
-//mailerexport
+// mailer export
 'LBL_MAILER_EXPORT'=>'郵寄導出',
 'LBL_MAILER_EXPORT_CONTACTS_TYPE'=>'選擇通訊錄:',
 'LBL_MAILER_EXPORT_CONTACTS_DESCR'=>'聯絡可能被選擇通過"自定義字段"和一些標準字段。.',
 'LBL_MAILER_EXPORT_RESULTS_TYPE'=>'選擇導出類型:',
-'LBL_MAILER_EXPORT_RESULTS_DESCR'=>'聚集從賬戶及其聯系人的數據,還給前次搜索.',
-'LBL_EXPORT_RESULTS_EMAIL'=>'導出郵件聯系人數據',
-'LBL_EXPORT_RESULTS_EMAIL_CORP'=>'導出郵件數據,如果聯系人"電郵"為空則用"公司電郵"',
-'LBL_EXPORT_RESULTS_FULL'=>'導出聯系人數據，電子郵件帳戶名稱，地址，電話等.',
-
+'LBL_MAILER_EXPORT_RESULTS_DESCR'=>'聚集從賬戶及其聯絡人的數據,還給前次搜索.',
+'LBL_EXPORT_RESULTS_EMAIL'=>'導出郵件聯絡人數據',
+'LBL_EXPORT_RESULTS_EMAIL_CORP'=>'導出郵件數據,如果聯絡人"電郵"為空則用"公司電郵"',
+'LBL_EXPORT_RESULTS_FULL'=>'導出聯絡人數據，電子郵件帳戶名稱，地址，電話等.',
 'LBL_EXPORT_RESULTS_GO'=>'導出',
 'LBL_MAILER_EXPORT_IGNORE'=>'--忽略--',
 'LBL_MAILER_EXPORT_CHECKED'=>'檢查',
 'LBL_MAILER_EXPORT_NOTCHECKED'=>'不進行檢查',
 
-//Addedafter5.0.4GA
+// Added after 5.0.4 GA
 
-//ModuleSequenceNumbering
-'AccountNo'=>'帳號',
+//Module Sequence Numbering
+'AccountNo'=>'帳戶號碼',
 //END
 
-//AccountHierarchy
-'LBL_SHOW_ACCOUNT_HIERARCHY'=>'查看賬號級別',
+// Account Hierarchy
+'LBL_SHOW_ACCOUNT_HIERARCHY'=>'查看帳號級別',
 
 );
 
