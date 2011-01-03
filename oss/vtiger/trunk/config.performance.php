b1267eb0acdd15488a81199b644d25549c1cae69
YuCheng Hu <yhu@isharkfly.com>
2011-01-03 07:33:11 +0000

    关闭所有错误提示，加快系统运行速度。
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@686 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

--- oss/vtiger/trunk/config.performance.php
+++ oss/vtiger/trunk/config.performance.php
@@ -10,31 +10,35 @@
 /* Performance paramters can be configured to fine tune vtiger CRM runtime */
 $PERFORMANCE_CONFIG = Array(
 	// Enable log4php debugging only if requried 
-	'LOG4PHP_DEBUG' => true,
+	'LOG4PHP_DEBUG' => false,
 
 	// Should the caller information be captured in SQL Logging?
 	// It adds little overhead for performance but will be useful to debug
-	'SQL_LOG_INCLUDE_CALLER' => true,
+	'SQL_LOG_INCLUDE_CALLER' => false,
 
 	// If database default charset is UTF-8, set this to true 
 	// This avoids executing the SET NAMES SQL for each query!
-	'DB_DEFAULT_CHARSET_UTF8' => true,
-
-	// To avoid overshooting max_connections configured for database server
-	// it is preferable to have this parameter set to false
-	'ALLOW_MULTI_DB_CONNECTION_PER_REQUEST' => false,
+	'DB_DEFAULT_CHARSET_UTF8' => false,
 
 	// Compute record change indication for each record shown on listview
-	'LISTVIEW_RECORD_CHANGE_INDICATOR' => true,
+	'LISTVIEW_RECORD_CHANGE_INDICATOR' => false,
 
 	// Turn-off default sorting in ListView, could eat up time as data grows
 	'LISTVIEW_DEFAULT_SORTING' => false,
 	
 	// Compute list view record count while loading listview everytime.
 	// Recommended value false
-	'LISTVIEW_COMPUTE_PAGE_COUNT' => true,
+	'LISTVIEW_COMPUTE_PAGE_COUNT' => false,
 
 	// Control DetailView Record Navigation
-	'DETAILVIEW_RECORD_NAVIGATION' => true,
+	'DETAILVIEW_RECORD_NAVIGATION' => false,
+
+	// To control the Email Notifications being sent to the Owner
+	'NOTIFY_OWNER_EMAILS' => false,		//By default it is set to true, if it is set to false, then notifications will not be sent
+	// reduce number of ajax requests on home page, reduce this value if home page widget dont
+	// show value.
+	'HOME_PAGE_WIDGET_GROUP_SIZE' => 12,
+	//take backup legacy style, whenever an admin user logs out.
+	'LOGOUT_BACKUP' => false,
 );
 ?>
