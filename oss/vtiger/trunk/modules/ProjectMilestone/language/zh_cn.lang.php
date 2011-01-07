1ce5a4e98b0d4126c184f8f0f23c91c51e063f3a
YuCheng Hu <yhu@isharkfly.com>
2011-01-07 07:50:43 +0000

    格式化注释代码。
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@690 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

--- oss/vtiger/trunk/modules/ProjectMilestone/language/zh_cn.lang.php
+++ oss/vtiger/trunk/modules/ProjectMilestone/language/zh_cn.lang.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Copyright (C) 2006-2010 YUCHENG HU
+ * Copyright (C) 2006-2011 YUCHENG HU
  *
  * ---------------------------------------------
  * HA WEBSYSTEMS
  * http://www.hawebs.net
- * https://www.hawebs.org/forums/computer/
+ * http://www.ossez.com
  *
  * CONTACT
  * huyuchengus@gmail.com / yuchenghu@hawebs.net
@@ -16,44 +16,44 @@
  *
  * ---------------------------------------------
  * NOTE
- * 1. 所有的语言配置文件请采用 UTF-8 编码 
+ * 1. 所有的 ini 文件必须采用无 BOM 的 UTF-8 编码
  *
  * ---------------------------------------------
  */
 $mod_strings = Array(
 	 
-	'LBL_MODULE_NAME'=>'Project Milestones',
-  	'LBL_MODULE_TITLE'=>'Project Milestones',
-	'LBL_NEW_OPERATION'=>'New Project Milestone',
-    'LBL_MILESTONES'=>'Project Milestones',
-    'LBL_MILESTONE'=>'Project Milestone',
-    'Project Milestone ID'=>'Milestone ID', 
-    'projectmilestones' => 'Project Milestones',
+	'LBL_MODULE_NAME'=>'项目里程碑',
+  	'LBL_MODULE_TITLE'=>'项目里程碑',
+	'LBL_NEW_OPERATION'=>'新项目里程碑',
+    'LBL_MILESTONES'=>'项目里程碑',
+    'LBL_MILESTONE'=>'项目里程碑',
+    'Project Milestone ID'=>'项目里程碑识别号', 
+    'projectmilestones' => '项目里程碑',
     //added this to translate the module name in the main app menu
-    'ProjectMilestone'=>'Project Milestones',
+    'ProjectMilestone'=>'项目里程碑',
     //
-    'SINGLE_ProjectMilestone' => 'Project Milestone',
-    'LBL_CUSTOM_INFORMATION' => 'Custom Information',
-    'LBL_PROJECT_MILESTONE_INFORMATION' => 'Project Milestone Information',
-    'LBL_DESCRIPTION_INFORMATION' => 'Description Information',
-    'Assigned To' => 'Assigned To',
-    'Milestone Date' => 'Milestone Date',
-    'Milestone' => 'Milestone',
-    'Type'=>'Type',
+    'SINGLE_ProjectMilestone' => '项目里程碑',
+    'LBL_CUSTOM_INFORMATION' => '自定义信息',
+    'LBL_PROJECT_MILESTONE_INFORMATION' => '项目里程碑信息',
+    'LBL_DESCRIPTION_INFORMATION' => '描述信息',
+    'Assigned To' => '指定给',
+    'Milestone Date' => '里程碑日期',
+    'Milestone' => '里程碑',
+    'Type'=>'类型',
     //'linktoproject'=>'Link To',
-    'Related Project' => 'Related Project',
-    'Related To' => 'Related To',
-    'projectid' => 'Related To',
-  	'administrative' => 'administrative',
-    'operative' => 'operative',
-    'other' => 'other',
-    'Created Time' => 'Created Time',
-    'Modified Time' => 'Modified Time',
-    'description' => 'Description',
-    'Description' => 'Description',
+    'Related Project' => '相关项目',
+    'Related To' => '相关于',
+    'projectid' => '相关于',
+  	'administrative' => '管理',
+    'operative' => '操作',
+    'other' => '其他',
+    'Created Time' => '创建时间',
+    'Modified Time' => '修改时间',
+    'description' => '描述',
+    'Description' => '描述',
     //added for related lists 
-    'Project Milestone Name' => 'Milestone Name',
-    'Project Milestone No' => 'Milestone No',
+    'Project Milestone Name' => '里程碑名字',
+    'Project Milestone No' => '里程碑编号',
 
 );
 
