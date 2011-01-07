8147bed07972449104cfa5b0ecc2bbc662e90e85
YuCheng Hu <yhu@isharkfly.com>
2011-01-07 15:34:25 +0000

    添加繁体中文到 SVN 库中。
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@694 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

---
+++ oss/vtiger/trunk/modules/uploads/language/zh_tw.lang.php
@@ -0,0 +1,35 @@
+<?php
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
+ * ("License"); You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * The Original Code is:  SugarCRM Open Source
+ * The Initial Developer of the Original Code is SugarCRM, Inc.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ ********************************************************************************/
+/*********************************************************************************
+ * $Header: /advent/projects/wesat/vtiger_crm/vtigercrm/modules/uploads/language/en_us.lang.php,v 1.1 2005/01/20 05:29:32 saraj Exp $
+ * Description:  Defines the English language pack
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________..
+ ********************************************************************************/
+
+$mod_strings = Array(
+
+//Added fields for uploads (attach files)
+'LBL_ATTACH_FILE' => '附件檔案',
+'LBL_ATTACH'=>'檔案',
+'LBL_CANCEL'=>'取消',
+ 'LBL_STEP_SELECT_FILE' => '步驟一：選擇檔案',
+ 'LBL_BROWSE_FILES' => '點選瀏覽器按鈕并且選擇要附加的檔案',
+  'LBL_DESCRIPTION' => '步驟二：填入一些描述',
+  'LBL_OPTIONAL' => '（選擇性輸入）',
+);
+
+?>
