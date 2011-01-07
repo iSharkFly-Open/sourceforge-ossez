d33dd489b7c721cea9f9ca7c6b5f9e5647e7fbcf
YuCheng Hu <yhu@isharkfly.com>
2011-01-07 15:35:36 +0000

    添加简体中文的注释。
    git-svn-id: https://svn.code.sf.net/p/hawebs/svn@695 a2543c7e-f6e9-4f8a-8bff-1ffc34733512

--- oss/vtiger/trunk/modules/uploads/language/zh_cn.lang.php
+++ oss/vtiger/trunk/modules/uploads/language/zh_cn.lang.php
@@ -1,25 +1,25 @@
 <?php
-/*********************************************************************************
- * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
- * ("License"); You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
- * Software distributed under the License is distributed on an  "AS IS"  basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- * The Original Code is:  SugarCRM Open Source
- * The Initial Developer of the Original Code is SugarCRM, Inc.
- * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
- * All Rights Reserved.
- * Contributor(s): ______________________________________.
- ********************************************************************************/
-/*********************************************************************************
- * $Header: /advent/projects/wesat/vtiger_crm/vtigercrm/modules/uploads/language/en_us.lang.php,v 1.1 2005/01/20 05:29:32 saraj Exp $
- * Description:  Defines the English language pack
- * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
- * All Rights Reserved.
- * Contributor(s): ______________________________________..
- ********************************************************************************/
-
+/**
+ * Copyright (C) 2006-2011 YUCHENG HU
+ *
+ * ---------------------------------------------
+ * HA WEBSYSTEMS
+ * http://www.hawebs.net
+ * http://www.ossez.com
+ *
+ * CONTACT
+ * huyuchengus@gmail.com / yuchenghu@hawebs.net
+ *
+ * ---------------------------------------------
+ * [A] GNU GENERAL PUBLIC LICENSE GNU/LGPL
+ * [B] Apache License, Version 2.0
+ *
+ * ---------------------------------------------
+ * NOTE
+ * 1. 所有的 ini 文件必须采用无 BOM 的 UTF-8 编码
+ *
+ * ---------------------------------------------
+ */
 $mod_strings = Array(
 
 //Added fields for uploads (attach files)
